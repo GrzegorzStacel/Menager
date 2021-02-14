3b97b0ec8c1d3faad2e204cf43c7333e2e031e37
Grzegorz Stącel <stacelgrzegorz@gmail.com>
2021-02-14 11:20:15 +0100

    Add route

--- startup/routes.js
+++ startup/routes.js
@@ -1,7 +1,7 @@
-const express = require('express');
 const index = require('../routes/index');
-
+const users = require('../routes/users');
 
 module.exports = function(app) {
     app.use('/', index);
+    app.use('/', users);
 }
