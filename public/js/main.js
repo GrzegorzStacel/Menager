b438534d03c7b382d38ed107be4f14468fe937bf
Grzegorz Stącel <stacelgrzegorz@gmail.com>
2021-05-17 18:08:28 +0200

    Change hamburger icon and add close animation

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,7 @@
-document.getElementsByClassName('header__hamburger')[0].addEventListener('click', function() {
+document.getElementsByClassName('header__hamburger')[0].addEventListener('click', function () {
     document.getElementsByClassName("header__ul")[0].classList.toggle('show')
+})
+
+document.getElementsByClassName('header__hamburger')[0].addEventListener('click', function () {
+    document.getElementsByClassName('bar')[0].classList.toggle('active')
 })
